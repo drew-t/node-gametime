fe3ff02c37ae6a5923f9d2644598299138830397
Drew Thompson <ferraritmnt@gmail.com>
2016-07-06 20:37:20 -0600

    use PORT constant

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+const PORT = process.env.PORT || 3000;
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
@@ -12,6 +14,6 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(80, function(){
-  console.log('listening on *:80');
+http.listen(PORT, function(){
+  console.log('listening on ', PORT);
 });
