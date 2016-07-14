dcce15504e091c56579a9725387d6d174dad7eef
Drew Thompson <ferraritmnt@gmail.com>
2016-07-13 21:27:16 -0600

    replacing with multi-player game served from node sever

--- index.js
+++ index.js
@@ -2,58 +2,55 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var players = {};
+var playerNumbers = [1, 2, 3, 4];
 var paddles = {
           '1': {player: 1, x: 10, y: (800 / 3), vertical: false},
-          '2': {player: 2, x: 790, y: (800 / 3), vertical: false},
+          '2': {player: 2, x: 730, y: (800 / 3), vertical: false},
           '3': {player: 3, x: 10, y: (800 / 3)*2, vertical: false},
-          '4': {player: 4, x: 790, y: (800 / 3)*2, vertical: false}
+          '4': {player: 4, x: 730, y: (800 / 3)*2, vertical: false}
 };
-var allPaddles = {}
 
 const PORT = process.env.PORT || 3000;
 
 io.on('connection', function(socket){
-  playerInfo(socket);
+  console.log('SOMEONE IS TRYING TO CONNECT!!!!!!', socket.client.id);
+ if (playerNumbers !== []){
+    playerInfo(socket);
+  }
   socket.on('initialize', function() {
-    // if (socket.conn.server.clientCount > 3){
-      socket.emit('initialize', players[socket.client.id]);
-    // }
+    socket.emit('initialize', players[socket.client.id]);
   });
   socket.on('paddle', function(paddle){
+    if (playerNumbers === []) {
+      io.emit('ready', paddles);
+    }
     paddles[paddle.player] = paddleLocation(paddle);
-    // console.log(paddles);
+    // console.log(players);
     io.emit('paddles', (paddles));
   });
   socket.on('disconnect', function() {
     delete players[socket.client.id];
+    playerNumbers.push(players[socket.client.id]);
+    // playerCount--;
   });
 });
 
 io.listen(PORT);
 
 function paddleLocation(paddleInfo) {
-  console.log(paddleInfo.player, paddleInfo.x, paddleInfo.y, paddleInfo.vertical);
   var paddleString = {
-          player: paddleInfo.player,
-          x: paddleInfo.x,
-          y: paddleInfo.y,
-          vertical: paddleInfo.vertical
+    player: paddleInfo.player,
+    x: paddleInfo.x,
+    y: paddleInfo.y,
+    vertical: paddleInfo.vertical
   };
   return paddleString;
 }
 
 function playerInfo(socket){
-    if (socket.conn.server.clientsCount === 1){
-      players[socket.client.id] = 1;
-    }
-    else if (socket.conn.server.clientsCount === 2){
-      players[socket.client.id] = 2;
-    }
-    else if (socket.conn.server.clientsCount === 3){
-      players[socket.client.id] = 3;
-    }
-    else if (socket.conn.server.clientsCount === 4){
-      players[socket.client.id] = 4;
-    }
-    return players[socket.client.id];
+  players[socket.client.id] = playerNumbers.pop();
+  console.log(players[socket.client.id]);
+  console.log(playerNumbers);
+
+  return players[socket.client.id];
 }
